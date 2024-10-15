56a93334cf6347664bf123932b0759de3aef0b99
hritikmittal09 <hritikmittal77@gmail.com>
2024-10-15 07:14:02 +0530

    added loading state

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -5,23 +5,42 @@ import HeroSection from './Header';
 
 function Blog() {
   const [allBlogs, setBlogs] = useState([]);
+  const [ loader , setLoader] = useState(true)
+
 
   const fetchBlogs = async () => {
     try {
       const res = await getBlogs();
+      setLoader(true)
       console.log(res.data); // Check if this is the correct data structure
       setBlogs(res.data);
     } catch (error) {
       console.error("Error fetching blogs:", error);
+   
     }
+    setLoader(false)
   };
 
   useEffect(() => {
     console.log("Blog component rendering...");
     fetchBlogs();
-  }, []); // Add empty dependency array to avoid infinite loop
+  }, []);
+   // Add empty dependency array to avoid infinite loop
+   if (loader) {
+    return (
+      <>
+      <div className=' flex justify-center items-center'>
+        <div className=' text-2xl m-3'>
+          Loading data....
+        </div>
 
-  return (
+      </div>
+      </>
+    )
+   }
+
+  
+   return (
     
     
     <div className='flex flex-col gap-5 justify-center items-center my-auto mt-10' >
